ddd5f1ee107fe6d5cef52cc318e9ac14ee4475b5
UgurcanGursen <33179697+UgurcanGursen@users.noreply.github.com>
2017-12-17 19:49:10 +0300

    Update SQliteHelper.java
    kanka sqlitehelper değiştirdim deneme123

--- app/src/main/java/com/example/fme/discover_istanbul/SQliteHelper.java
+++ app/src/main/java/com/example/fme/discover_istanbul/SQliteHelper.java
@@ -19,13 +19,15 @@ public class SQliteHelper extends SQLiteOpenHelper {
     private static final String table_SPORTS = "sporlar";
     private static final String sport_ID = "id";
     private static final String sport_TITLE = "baslik";
+    private static final String sport_KATEGORI = "kategori";
     private static final String sport_AUTHOR = "yazar";
-    private static final String[] COLUMNS = {sport_ID, sport_TITLE, sport_AUTHOR};
+    private static final String[] COLUMNS = {sport_ID, sport_TITLE, sport_AUTHOR,sport_KATEGORI};
     private static final String CREATE_SPORT_TABLE = "CREATE TABLE "
             + table_SPORTS+ " ("
             + sport_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + sport_TITLE+ " TEXT, "
-            + sport_AUTHOR+ " TEXT )";
+            + sport_AUTHOR+ " TEXT,"
+            + sport_KATEGORI + " TEXT ) ";
 
     public SQliteHelper(Context context) {
 
@@ -49,6 +51,7 @@ public class SQliteHelper extends SQLiteOpenHelper {
         ContentValues degerler = new ContentValues();
         degerler.put(sport_TITLE, sport.getBaslik());
         degerler.put(sport_AUTHOR, sport.getYazar());
+        degerler.put(sport_KATEGORI,sport.getKategori());
         sqLiteDatabase.insert(table_SPORTS,null,degerler);
         sqLiteDatabase.close();
 
@@ -65,6 +68,7 @@ public class SQliteHelper extends SQLiteOpenHelper {
                 sport.setId(Integer.parseInt(cursor.getString(0)));
                 sport.setBaslik(cursor.getString(1));
                 sport.setYazar(cursor.getString(2));
+                sport.setKategori(cursor.getString(3));
                 sporlar.add(sport);
 
             }while (cursor.moveToNext());
@@ -91,6 +95,25 @@ public class SQliteHelper extends SQLiteOpenHelper {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         sqLiteDatabase.delete(table_SPORTS,sport_ID + " = ?",new String[]{String.valueOf(sport.getId())});
         sqLiteDatabase.close();
+    }
+
+    public List<Sport> futbolGetir(){
+        List<Sport> sporlar = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.query(table_SPORTS,COLUMNS,sport_KATEGORI + " = 'Futbol'",null,null,null,null);
+        Sport sport = null;
+        if (cursor.moveToFirst()){
+            do {
+                sport = new Sport();
+                sport.setId(Integer.parseInt(cursor.getString(0)));
+                sport.setBaslik(cursor.getString(1));
+                sport.setYazar(cursor.getString(2));
+                sport.setKategori(cursor.getString(3));
+                sporlar.add(sport);
+
+            }while (cursor.moveToNext());
+        }
+        return sporlar;
     }
 
 
